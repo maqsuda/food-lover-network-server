84b0e98476d0c38abf59b542dfc63ee3f70ddfe3
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2025-11-12 18:23:39 +0600

    added my favorite

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,8 +8,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://foodReviewDB:8INEU3elW8KzA1IT@cluster0.tbo2m.mongodb.net/?appName=Cluster0";
+// const uri =
+//   "mongodb+srv://foodReviewDB:8INEU3elW8KzA1IT@cluster0.tbo2m.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tbo2m.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,6 +34,7 @@ async function run() {
     const db = client.db("food_reviews");
     const usersCollection = db.collection("users");
     const reviewCollection = db.collection("reviews");
+    const favoriteCollection = db.collection("favorites");
 
     app.post("/users", async (req, res) => {
       const newUser = req.body;
@@ -53,7 +56,12 @@ async function run() {
     });
 
     app.get("/all-reviews", async (req, res) => {
-      const cursor = reviewCollection.find().sort({ reviewDate: -1 });
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+      const cursor = reviewCollection.find(query).sort({ reviewDate: -1 });
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -65,9 +73,60 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/all-reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/all-reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          foodName: updateData.foodName,
+          foodPhoto: updateData.foodPhoto,
+          restaurantName: updateData.restaurantName,
+          restaurantLocation: updateData.restaurantLocation,
+          rating: updateData.rating,
+          reviewDate: updateData.reviewDate,
+          comments: updateData.comments,
+        },
+      };
+      const options = {};
+      const result = await reviewCollection.updateOne(query, update, options);
+      res.send(result);
+    });
+
     app.post("/addReviews", async (req, res) => {
       const newReview = req.body;
       const result = await reviewCollection.insertOne(newReview);
+      res.send(result);
+    });
+
+    app.post("/addFavorite", async (req, res) => {
+      const newFavorite = req.body;
+      const result = await favoriteCollection.insertOne(newFavorite);
+      res.send(result);
+    });
+
+    app.delete("/all-favorite/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await favoriteCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/all-favorite", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+      const cursor = favoriteCollection.find(query).sort({ reviewDate: -1 });
+      const result = await cursor.toArray();
       res.send(result);
     });
 
